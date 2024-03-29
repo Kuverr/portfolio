210d89dbb9ab9756f958e3747596ec3e3d3e1ba3
Kuver <ziutek8921@gmail.com>
2024-03-29 20:39:46 +0100

    Initial

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ window.onload = () => {
     setTimeout(() => {
         startCycle();
     }, 2000);
+
+    backgroundTest();
 };
 
 
@@ -19,6 +21,7 @@ let cyclerContent = [
     'loga',
     'z pasji',
 ];
+
 function startCycle() {
 
     let currentContent = cyclerEl.textContent;
@@ -49,4 +52,25 @@ function startCycle() {
     setTimeout(() => {
         startCycle();
     }, 5000);
+}
+
+function backgroundTest() {
+    //document width and scroll height
+    let docWidth = document.documentElement.scrollWidth;
+    let docHeight = document.documentElement.scrollHeight;
+
+    let svg = ` <svg xmlns="http://www.w3.org/2000/svg" width="${docWidth}" height="${docHeight}">`;
+    for (let i = 0; i < 20; i++) {
+
+
+        let x1 = Math.floor(Math.random() * docWidth);
+        let y1 = Math.floor(Math.random() * docHeight);
+        let x2 = Math.floor(Math.random() * docWidth);
+        let y2 = Math.floor(Math.random() * docHeight);
+
+        svg += `<line x1="${x1}" y1="${y1}" x2="${x2}" y2="${y2}" style="stroke:rgb(0, 123, 255);stroke-width:2" />`;
+    }
+    svg += `</svg>`;
+
+    document.querySelector('main').style.backgroundImage = `url('data:image/svg+xml;utf8,${encodeURIComponent(svg)}')`;
 }
